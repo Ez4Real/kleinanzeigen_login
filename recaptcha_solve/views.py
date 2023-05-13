0ed19f4814ecb162848661289977a035958cf931
Ez4Real <butilka05roma@gmail.com>
2023-05-13 19:27:51 +0300

    onwork''

--- recaptcha_solve/views.py
+++ recaptcha_solve/views.py
@@ -1,4 +1,3 @@
-import requests
 import time
 
 from django.shortcuts import render
@@ -12,11 +11,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
-
+from selenium.webdriver.remote.webelement import WebElement
 
 from .forms import LoginForm
 
-from selenium.webdriver.remote.webelement import WebElement
 
 
 def find_element(driver: webdriver.Chrome,
@@ -58,7 +56,10 @@ class LoginView(View):
             page_url = settings.LOGIN_PAGE_URL
             # page_url = 'https://www.google.com/recaptcha/api2/demo'
             
-            driver = webdriver.Chrome()
+    
+            driver = webdriver.Chrome(keep_alive=True)
+            
+            driver = webdriver.Edge(keep_alive=True)
             driver.get(page_url)
 
             # Close modals
@@ -78,66 +79,24 @@ class LoginView(View):
             # Get reCAPTCHA audio file
             button_click(driver, By.CLASS_NAME, 'recaptcha-checkbox')
             
+            time.sleep(3)
+            
             driver.switch_to.default_content()
             frame = driver.find_element(By.XPATH, "//iframe[@title='reCAPTCHA-Aufgabe läuft in zwei Minuten ab']")
+            # frame = driver.find_element(By.XPATH, "//iframe[@title='текущую проверку reCAPTCHA можно пройти в течение ещё двух минут']")
             frames.append(frame)
-            
             
             driver.switch_to.frame(frame)
             
             button_click(driver, By.CLASS_NAME, 'rc-button-audio')
+            time.sleep(3)
+            
+            
             button_click(driver, By.CLASS_NAME, 'rc-button-default')
+            time.sleep(3)
             
-            download = find_element(driver, By.CLASS_NAME, 'rc-audiochallenge-tdownload-link')
-            print(download.get_attribute('src'))
+            audiofile_path = find_element(driver, By.CLASS_NAME, 'rc-audiochallenge-tdownload-link')
             
-
-            # solver = TwoCaptcha(settings.API_KEY)
-            
-            # print('\n', solver.balance())
-        
-
-            # try:
-            #     result = solver.recaptcha(
-            #         sitekey=settings.DATA_SITE_KEY,
-            #         url=settings.LOGIN_PAGE_URL)
-
-            # except Exception as e:
-            #     print("An exception occurred while solving the CAPTCHA:")
-            #     print("Error type:", type(e).__name__)
-            #     print("Error message:", str(e))
-
-            # else:
-            #     print('solved: ' + str(result))
-            
-            # response = requests.post('https://captchasolver.com/api/recaptcha',
-            #                          json={
-            #     "url": settings.LOGIN_PAGE_URL,
-            #     "apikey": settings.API_KEY,
-            #     "sitekey": settings.DATA_SITE_KEY
-            # })
-
-            # if response.ok:
-            #     result = response.json()
-            #     if result['status'] == 1:
-            #         print('CAPTCHA solved: {}'.format(result['solution']['gRecaptchaResponse']))
-            #     else:
-            #         print('CAPTCHA was not solved')
-            # else:
-            #     print('Error occurred while solving CAPTCHA: {}'.format(response.text))
-                
-                
-            
-            
-            
-
-            # Submit the login form with the ReCAPTCHA response
-            # login_data = {
-            #     'username': 'myusername',
-            #     'password': 'mypassword',
-            #     'g-recaptcha-response': captcha_response
-            # }
-            # login_response = requests.post(page_url, data=login_data)
             
             
             # button_click(driver, By.ID, 'login-submit')
